a1d524bceb3b4fa513bfca5334964e57d5590270
unknown <augustboesen@gmail.com>
2024-09-03 09:56:13 +0300

    commented footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -11,30 +11,35 @@ export default function Footer() {
   return (
     <footer>
       <div className={isFollowing ? 'active' : ''}>
+        {/* Button for revealing and hiding the corner card */}
         <button className='hover:text-slate-900' onClick={toggleFollow}>
           {' '}
           {isFollowing ? 'x' : 'Contact me'}
         </button>
         {isFollowing && (
           <ul className='rotate-90 ml-12 -mt-8 scale-50 text-sm flex flex-row'>
+            {/* Instagram */}
             <li>
               <SocialIcon
                 className='ml-1 max-w-9'
                 url='https://www.instagram.com/jytkypappa555/?hl=fi'
               />
             </li>
+            {/* LinkedIn */}
             <li>
               <SocialIcon
                 className='ml-1 max-w-9'
                 url='https://www.linkedin.com/in/severi-boesen-71a0b31a7/'
               />
             </li>
+            {/* YouTube */}
             <li>
               <SocialIcon
                 className='ml-1 max-w-9'
                 url=' 	www.youtube.com/@scionicmusic'
               />
             </li>
+            {/* GitHub */}
             <li>
               <SocialIcon
                 className='ml-1 max-w-9'
@@ -43,6 +48,7 @@ export default function Footer() {
             </li>
           </ul>
         )}
+        {/* Card content while revealed */}
         {isFollowing && (
           <ul className='whitespace-nowrap rotate-90 ml-28 -mt-[4.7rem] scale-50 text-[0.6rem] flex flex-col'>
             <li>
