e89285e37a00b3f466bda78729123aeb09b94aa7
unknown <augustboesen@gmail.com>
2024-12-12 11:02:22 +0200

    updated navigation and styled better

--- app/ticorporate/page.tsx
+++ app/ticorporate/page.tsx
@@ -14,32 +14,22 @@ const Ticorporate: NextPage = () => {
         height={275}
       />
       <h1 className='text-teal-500 font-bold mb-16 text-center'>PACKY</h1>
-      <div className='flex justify-evenly w-full'>
-        <Link href='/'>Etusivulle</Link>
-        <Link href='https://jamk-packy.netlify.app/' target='_blank'>
-          Sovellus
-        </Link>
-        <Link href='https://github.com/jamktiko/PACKY' target='_blank'>
-          GitHub
-        </Link>
-      </div>
-
-      <div className='bg-cyan-950 bg-opacity-30'>
-        <h2>Projektin yleiskuva</h2>
+      <div className='bg-cyan-950 bg-opacity-30 px-4'>
+        <h2 className='text-teal-300 mt-16'>Projektin yleiskuva</h2>
         <p className=''>
-          PACKY on erityisesti aloitteleville sovelluskehittäjille suunnattu
-          apu/työkalusovellus, joka tarjoaa teknologiaratkaisuja käyttäjän
-          preferenssien sekä tarpeiden mukaan.
+          <b>PACKY on erityisesti</b> aloitteleville sovelluskehittäjille
+          suunnattu apu/työkalusovellus, joka tarjoaa teknologiaratkaisuja
+          käyttäjän preferenssien sekä tarpeiden mukaan.
         </p>
         <p>
-          Alkuperäinen visio toteutettiin vuoden 2024 keväällä Software
+          <b>Alkuperäinen visio</b> toteutettiin vuoden 2024 keväällä Software
           Engineering-opintojakson lopputyönä, jolloin tiimi koostui pitkälti
           eri jäsenistä ja sovelluksen visio oli teknisen sekä käytännön
           toteutuksen suhteen erilainen.
         </p>
         <h3>Roolini projektissa</h3>
         <p>
-          Syksyn aikana pääasialliset roolini olivat tuoteomistaja sekä
+          <b>Syksyn aikana pääasialliset</b> roolini olivat tuoteomistaja sekä
           UI/UX-kehittäjä. Tuoteomistajan roolissa kehitin sovelluksen vision,
           olin merkittävässä roolissa sen sovellusarkkitehtuurisissa
           ratkaisuissa ja ylläpidin projektin oikeansuuntaista etenemistä. UI:n
@@ -55,34 +45,35 @@ const Ticorporate: NextPage = () => {
           height={500}
         />
       </div>
-      <div className='bg-teal-950 bg-opacity-30'>
-        <h2>Ennen projektia</h2>
+      <div className='bg-teal-950 bg-opacity-30 px-4'>
+        <h2 className='text-cyan-300 mt-16'>Ennen projektia</h2>
         <p>
-          Koska PACKY oli jo prototypioitu kevään opintojaksossa, sen perustat
-          olivat todettu toimiviksi. Ajatuksena oli jatkokehittää sen
+          <b>Koska PACKY oli jo prototypioitu</b> kevään opintojaksossa, sen
+          perustat olivat todettu toimiviksi. Ajatuksena oli jatkokehittää sen
           potentiaalia aputyökaluna opintojaan suorittaville
           sovelluskehittäjille (esimerkiksi 1. vuoden tietojenkäsittelyn
           opiskelijoille).
         </p>
         <h3>Lähtötaidot</h3>
         <p>
-          Ennen Ticorporate-toteutusta osasin suunnitella, toteuttaa ja soveltaa
-          UI/UX-puolta mielestäni ammattitasoisesti. Sovelluskehitysprojektista
-          tiimitasolla oli vain kevään Software Engineering-opintojakson verran
-          aiempaa kokemusta, joten työskentely pitkäjänteisesti yhden
-          sovelluksen parissa tuli uutena. En projektin alussa tiennyt, miten
-          hahmotella sovelluskehitysprosessin vaiheita tällä skaalalla ja
-          halusin oppia paljon tähän liittyviä metodeja.
+          <b>Ennen Ticorporate-toteutusta</b> osasin suunnitella, toteuttaa ja
+          soveltaa UI/UX-puolta mielestäni ammattitasoisesti.
+          Sovelluskehitysprojektista tiimitasolla oli vain kevään Software
+          Engineering-opintojakson verran aiempaa kokemusta, joten työskentely
+          pitkäjänteisesti yhden sovelluksen parissa tuli uutena. En projektin
+          alussa tiennyt, miten hahmotella sovelluskehitysprosessin vaiheita
+          tällä skaalalla ja halusin oppia paljon tähän liittyviä metodeja.
         </p>
         <h3>Visio</h3>
         <p>
-          PACKYN visio oli yksinkertainen, mutta monivaiheinen ja skaalaltaan
-          mittava. Sovellusta toteuttaessa halusin ottaa huomioon sen, miten
-          käyttäjäpolku strukturoituu, sillä hypoteettisena jatkokehityksenä
-          tietokantamuutoksilla halusin olevan mahdollista sisällyttää myös
-          muita sovelluskehityksen osa-alueita web-sovelluskehityksen lisäksi.
-          Esimerkiksi uudella tietokantakokonaisuudella PACKY voisi antaa
-          ohjeistusta aloitteleville pelinkehittäjille.
+          <b>PACKYN visio oli yksinkertainen</b>, mutta monivaiheinen ja
+          skaalaltaan mittava. Sovellusta toteuttaessa halusin ottaa huomioon
+          sen, miten käyttäjäpolku strukturoituu, sillä hypoteettisena
+          jatkokehityksenä tietokantamuutoksilla halusin olevan mahdollista
+          sisällyttää myös muita sovelluskehityksen osa-alueita
+          web-sovelluskehityksen lisäksi. Esimerkiksi uudella
+          tietokantakokonaisuudella PACKY voisi antaa ohjeistusta aloitteleville
+          pelinkehittäjille.
         </p>
         <Image
           className='mx-auto shadow-inner pb-6 mt-2 border border-slate-800'
@@ -92,10 +83,10 @@ const Ticorporate: NextPage = () => {
           height={500}
         />
       </div>
-      <div className='bg-blue-950 bg-opacity-30'>
-        <h2>Projektin aikana</h2>
+      <div className='bg-blue-950 bg-opacity-30 px-4'>
+        <h2 className='text-blue-300 mt-16'>Projektin aikana</h2>
         <p>
-          Projektin aikana suuri osa (n. 75%) oli UI/UX-työskentelyä tai
+          <b>Projektin aikana suuri osa</b> (n. 75%) oli UI/UX-työskentelyä tai
           frontend-toteutusta. Koodin lisäksi käytin aikaa projektin visiointiin
           (käyttäjäpolun iterointiin), dokumentaatioon (GitHubin README:n
           kirjoittamiseen) ja TiCorporaten muihin puoliin (Business
@@ -103,31 +94,32 @@ const Ticorporate: NextPage = () => {
         </p>
         <h3>Suunnittelu</h3>
         <p>
-          Suunnittelu toteutui pitkälti kolmella eri painopisteellä. Software
-          Engineering-opintojaksolla sovellusta oltiin päästy prototypioimaan ja
-          tämän pohjalta jatkokehittämään. Tämän jatkokehityksen ollessa
-          ensimmäisessä demossa valmis uudistimme palautteen perusteella
-          sovelluksen toimivuuden. Tämän jälkeen suunnittelu tapahtui tiimin
-          kesken ja koski pitkälti sovelluksen struktuuria sekä toteutuksellisia
-          valintoja esimerkiksi funktioissa ja tilanhallinnallisissa
-          ratkaisuissa.
+          <b>Suunnittelu toteutui pitkälti</b> kolmella eri painopisteellä.
+          Software Engineering-opintojaksolla sovellusta oltiin päästy
+          prototypioimaan ja tämän pohjalta jatkokehittämään. Tämän
+          jatkokehityksen ollessa ensimmäisessä demossa valmis uudistimme
+          palautteen perusteella sovelluksen toimivuuden. Tämän jälkeen
+          suunnittelu tapahtui tiimin kesken ja koski pitkälti sovelluksen
+          struktuuria sekä toteutuksellisia valintoja esimerkiksi funktioissa ja
+          tilanhallinnallisissa ratkaisuissa.
         </p>
         <h3>Toteutus</h3>
         <p>
-          Projektissa työstöni voidaan jakaa karkeasti kolmeen osaan. Osuuteni
-          tuoteomistajana oli varmistaa, että projekti toteutuu visiolleen
-          uskollisesti. Tämä vaati suunnittelua sekä hahmottelua tiimin kanssa
-          kommunikoiden. UI/UX-suunnittelijana suunnittelin sekä implementoin
-          sovelluksen ulkomuodon kokonaisuudessaan - olin vastuussa
+          <b>Projektissa työstöni voidaan jakaa</b> karkeasti kolmeen osaan.
+          Osuuteni tuoteomistajana oli varmistaa, että projekti toteutuu
+          visiolleen uskollisesti. Tämä vaati suunnittelua sekä hahmottelua
+          tiimin kanssa kommunikoiden. UI/UX-suunnittelijana suunnittelin sekä
+          implementoin sovelluksen ulkomuodon kokonaisuudessaan - olin vastuussa
           komponenttien järjestyksestä, siitä miltä kaikki näyttää ja miten
           design ilmenee koodissa.
         </p>
         <p>
-          Näiden kahden lisäksi toteutin sovelluksen frontendiä yhdessä muiden
-          tiimiläisten kanssa. Esimerkiksi sovelluksen käyttäjäpolussa toinen
-          vaihe (grid-näkymä, jossa featuret valitaan) on omaa käsialaani, sillä
-          myös UI-design-perspektiivistä koin tärkeäksi sen toteuttamisen juuri
-          siinä muodossa, jollaisena se nykymuodossaankin toimii.
+          <b>Näiden kahden lisäksi</b> toteutin kolmantena sovelluksen
+          frontendiä yhdessä muiden tiimiläisten kanssa. Esimerkiksi sovelluksen
+          käyttäjäpolussa toinen vaihe (grid-näkymä, jossa featuret valitaan) on
+          omaa käsialaani, sillä myös UI-design-perspektiivistä koin tärkeäksi
+          sen toteuttamisen juuri siinä muodossa, jollaisena se
+          nykymuodossaankin toimii.
         </p>
         <Image
           className='mx-auto shadow-inner pb-6 mt-2 border border-slate-800'
@@ -137,36 +129,55 @@ const Ticorporate: NextPage = () => {
           height={500}
         />
       </div>
-      <div className='bg-purple-950 bg-opacity-30'>
-        <h2>Projektin jälkeen</h2>
+      <div className='bg-purple-950 bg-opacity-30 px-4'>
+        <h2 className='text-purple-300 mt-16'>Projektin jälkeen</h2>
         <p>
-          PACKYn työstö oli minulle henkilökohtaisesti monelta osinvalaisevaa ja
-          työstön päätteeksi olen tyytyväinen annettuun panokseen niin omalta
-          kuin koko tiimin osalta. Henkilökohtainen näkemykseni on, että
-          työpanos jakautui todella hyvin ja jokaisen henkilökohtaisen
-          osaamisen/kokemustason osilta olimme jopa onnekkaita.
+          <b>PACKYn työstö oli minulle henkilökohtaisesti</b> monelta
+          osinvalaisevaa ja työstön päätteeksi olen tyytyväinen annettuun
+          panokseen niin omalta kuin koko tiimin osalta. Henkilökohtainen
+          näkemykseni on, että työpanos jakautui todella hyvin ja jokaisen
+          henkilökohtaisen osaamisen/kokemustason osilta olimme jopa onnekkaita.
         </p>
         <h3>Osaamisen nykytilanne</h3>
         <p>
-          Vaikka TiCorporate ei antanut minulle UI/UX-toteuttamisen osilta
-          juurikaan mitään uutta, otan sen pitkäjänteisen työstämisen silti
-          oppimiskokemuksena itseisarvoisesti. Eniten opin projektityöstöstä,
-          sen vaiheistamisesta, tämänkokoisen projektin teknologioiden skaalasta
-          sekä monesta huomionarvoisesta asiasta, kuten tilanhallinnasta, joista
-          en aiemmin omannut juurikaan kokemusta tai ymmärrystä.
+          <b>Vaikka TiCorporate ei antanut</b> minulle UI/UX-toteuttamisen
+          osilta juurikaan mitään uutta, otan sen pitkäjänteisen työstämisen
+          silti oppimiskokemuksena itseisarvoisesti. Eniten opin
+          projektityöstöstä, sen vaiheistamisesta, tämänkokoisen projektin
+          teknologioiden skaalasta sekä monesta huomionarvoisesta asiasta, kuten
+          tilanhallinnasta, joista en aiemmin omannut juurikaan kokemusta tai
+          ymmärrystä.
         </p>
         <h3>Itsereflektio</h3>
         <p>
-          TiCorporate-työpanokseni tuntimäärällisesti oli noin 340 työtuntia.
-          Kipeänä olin syksyn aikana noin 8 arkipäivää, mikä tarkoittaa
-          karkeasti noin 40 työtuntia. Omien opintojeni lisäksi olen ollut
-          aktiivina sekä loppuvuodesta myös hallituslaisena JAMKO:ssa tähän
-          kulunut aika on kokonaisuudessaan myös noin 45 työtuntia.
+          <b>TiCorporate-työpanokseni tuntimäärällisesti</b> oli noin 340
+          työtuntia. Kipeänä olin syksyn aikana noin 8 arkipäivää, mikä
+          tarkoittaa karkeasti noin 40 työtuntia. Omien opintojeni lisäksi olen
+          ollut aktiivina sekä loppuvuodesta myös hallituslaisena JAMKO:ssa
+          tähän kulunut aika on kokonaisuudessaan myös noin 45 työtuntia. Näiden
+          lisäksi voidaan laskea noin 5 yksittäistä päivää (esimerkiksi
+          Kauppakadun Approjen jälkeinen perjantai), jolloin pidimme tiimin
+          kesken yhteisesti sovittuna vapaapäivän.
+        </p>
+        <p>
+          Olen hyvin tyytyväinen antamaani panokseen ja koen tehneeni oman
+          osuuteni esimerkillisesti. Myös aihiot, joita tulin opettelemaan
+          TiCorporateen esimerkiksi projektityöstön suhteen, toteutuivat
+          mielestäni todella hyvin. Olen tyytyväinen kädenjälkeeni sovelluksen
+          ulkomuodossa ja näen siinä myös edelleen jatkokehitysideoita, joiden
+          avulla pidemmällä aikavälillä voisin parannella nykyistä mallia.
+        </p>
+        <p>
+          Kehittäisin työstössäni omaa kärsivällisyyttäni muiden kanssa
+          työskennellessä ja parantaisin omaa kommunikointiani. Henkilökohtaisen
+          jaksamisen suhteen voisin tauottaa työskentelyäni tasavälisemmin ja
+          pyytämään aina tarvittaessa apua työkavereilta. Näiltä osin ei ollut
+          ongelmia, mutta näen en jatkuvan kehityksen kohteina.
         </p>
         <h3>Tulevaisuus</h3>
         <p>
-          PACKYn jälkeen koen olevani avoimempi tekemään työkseni UI/UX:ää
-          tulevaisuudessa. Olen edelleen myös kiinnostunut
+          <b>PACKYn jälkeen koen</b> olevani avoimempi tekemään työkseni
+          UI/UX:ää tulevaisuudessa. Olen edelleen myös kiinnostunut
           sovellusarkkitehtuurista sekä fullstack-työstämisestä, joiden saralla
           olen edelleen asennoitunut työskentelemään korkeamman palkkatason
           vuoksi, mutta mahdollisuuksien mukaan aion ylläpitää omia
@@ -174,12 +185,35 @@ const Ticorporate: NextPage = () => {
           tätä vahvuusaluetta hyödykseni tulevaisuudessa, kun haen töitä.
         </p>
         <p className='pb-16'>
-          TiCorporaten myötä olen myös alkanut pohtimaan opinnäytetyöaiheita,
-          sillä UI/UX voisi olla itselleni hyvinkin mieleinen aihealue.
-          Vaihtoehtoisesti voisin perehtyä esimerkiksi sovelluskehysten välisiin
-          eroihin, hyödynnettäviin painopisteisiin tai laajemmalla skaalalla
-          teknologiapinojen suunnitteluun sekä huomionarvoisiin kohtiin.
+          <b>TiCorporaten myötä olen</b> myös alkanut pohtimaan
+          opinnäytetyöaiheita, sillä UI/UX voisi olla itselleni hyvinkin
+          mieleinen aihealue. Vaihtoehtoisesti voisin perehtyä esimerkiksi
+          sovelluskehysten välisiin eroihin, hyödynnettäviin painopisteisiin tai
+          laajemmalla skaalalla teknologiapinojen suunnitteluun sekä
+          huomionarvoisiin kohtiin.
         </p>
+      </div>
+      <div className='flex justify-evenly w-full py-4 bg-slate-950 border-x border-slate-800'>
+        <Link
+          className='border w-full text-center border-teal-500 bg-teal-950 bg-opacity-80 py-2 mx-1 rounded-2xl'
+          href='/'
+        >
+          Etusivulle
+        </Link>
+        <Link
+          className='border w-full text-center border-teal-500 bg-teal-950 bg-opacity-80 py-2 mx-1 rounded-2xl'
+          href='https://jamk-packy.netlify.app/'
+          target='_blank'
+        >
+          Sovellus
+        </Link>
+        <Link
+          className='border w-full text-center border-teal-500 bg-teal-950 bg-opacity-80 py-2 mx-1 rounded-2xl'
+          href='https://github.com/jamktiko/PACKY'
+          target='_blank'
+        >
+          GitHub
+        </Link>
       </div>
     </div>
   );
